19d84e8cb7850cc548a98f3779376fd9d65e8bdd
Makrand <makrandshingare@gmail.com>
2022-04-10 21:00:05 +0530

    Added : PUT Request Method and pass first name as Path Variable and last name as Query Parameter

--- src/main/java/com/biz/mesapp/HelloRESTAPIMessagingApp/controller/HelloMessages.java
+++ src/main/java/com/biz/mesapp/HelloRESTAPIMessagingApp/controller/HelloMessages.java
@@ -3,27 +3,42 @@ package com.biz.mesapp.HelloRESTAPIMessagingApp.controller;
 import com.biz.mesapp.HelloRESTAPIMessagingApp.entity.User;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloMessages {
-    @RequestMapping(value = {"hello","\n","/"})
-    public String printHello(){
+    //UC 1
+    @RequestMapping(value = {"hello", "\n", "/"})
+    public String printHello() {
         return "Hello from Bridgelabz";
     }
 
-    @RequestMapping(value = {"query","\n","/"})
-    public String printUsingQueryHello(@RequestParam(value = "name") String name){
-        return "Hello "+name+" from Bridgelabz";
+    //UC 2
+    @RequestMapping(value = {"query", "\n", "/"})
+    public String printUsingQueryHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from Bridgelabz";
     }
 
-    @PutMapping( "/put/{firstName}")
-    public String sayHello ( @PathVariable String firstName,
-                             @RequestParam(value = "lastname") String lastName) {
-        return "Hello" + firstName + " " + lastName+ " !";
+
+    //UC 3 Request Method and pass name as path variable
+    @GetMapping("/param/{name}")
+    public String syaHello(@PathVariable Stroke name) {
+        return "Hello " + name + " !";
     }
+
+    //UC 4
     @PostMapping("/post")
-    public String sayHello(@RequestBody User user){
-        return "hello "+user.getFirstName()+" "+user.getLastName()+"!";
+    public String sayHello(@RequestBody User user) {
+        return "hello " + user.getFirstName() + " " + user.getLastName() + "!";
     }
+
+    //UC 5
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastname") String lastName) {
+        return "Hello" + firstName + " " + lastName + " !";
+    }
+
 
 }
